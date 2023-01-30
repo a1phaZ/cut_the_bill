5dc0f29d88c423779d06bfb3f3f11d446b78a450
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2023-01-30 11:39:11 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cut_the_bill',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
